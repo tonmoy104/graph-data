2fbbd5836ab238a93bfe0f62dc1a5dc0eb8780c9
Zahid Hasan Alvi <zahid.hasan@bs-23.net>
2022-01-13 20:27:39 +0600

    Setup for production deployment on heroku

---
+++ server.js
@@ -0,0 +1,23 @@
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+const express = require('express');
+const app = express();
+if (process.env.NODE_ENV === 'production') {
+    app.use(requireHTTPS);
+}
+
+app.use(express.static("./dist/graph-data"));
+
+app.get("/*", function(req, res) {
+    res.sendFile("index.html", {root: "dist/graph-data"});
+});
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log("Server started on port " + port);
